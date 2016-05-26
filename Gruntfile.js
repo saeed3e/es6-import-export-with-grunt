64643994d323d84420c7771e745fadb34cd18f95
mohd.saeed <mohd.saeed@naukri.com>
2016-05-26 17:33:11 +0530

    update config file.......

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,6 +17,6 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.file.expand('./../node_modules/grunt-*/tasks').forEach(grunt.loadTasks);
-    require('./../node_modules/grunt-config-merge')(grunt);
+    grunt.file.expand('./node_modules/grunt-*/tasks').forEach(grunt.loadTasks);
+    require('./node_modules/grunt-config-merge')(grunt);
 };
